2852432d04a1eaeafc5af5121674051cc411dc45
Kari O'Neal <triadfirewood@gmail.com>
2019-06-02 20:14:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 3, 'Male', 'Brown', 'calm', true)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES()
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES()
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES()
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES()
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES()
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES()
+
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES()
