5b72908db4c32e1ad1b7ce97bd03ab3cbb833fcd
Kari O'Neal <triadfirewood@gmail.com>
2019-06-02 20:19:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 3, 'Male', 'Brown', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 3, 'Male', 'Brown', 'calm', true);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Rowdy', 1, 'Male', 'Black', 'wild', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Rowdy', 1, 'Male', 'Black', 'wild', true);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Tabitha', 9, 'Female', 'Brown', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Tabitha', 9, 'Female', 'Brown', 'calm', true);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Sergent Brown', 12, 'Male', 'Gray', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Sergent Brown', 12, 'Male', 'Gray', 'calm', true);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Melissa', 4, 'Female', 'White', 'sweet', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Melissa', 4, 'Female', 'White', 'sweet', true);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Grinch', 22, 'Male', 'Gray', 'grumpy', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Grinch', 22, 'Male', 'Gray', 'grumpy', true);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Wendy', 8, 'Female', 'Gray', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Wendy', 8, 'Female', 'Gray', 'calm', true);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES(NULL, 3, 'Male', 'Brown', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES(NULL, 3, 'Male', 'Brown', 'calm', true);
