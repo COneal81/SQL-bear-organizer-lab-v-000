33b9890352d608f36b83ed473d2b49bd6da82774
Kari O'Neal <triadfirewood@gmail.com>
2019-06-02 20:18:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,10 +6,10 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Tabitha
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Sergent Brown', 12, 'Male', 'Gray', 'calm', true)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 3, 'Male', 'Brown', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Melissa', 4, 'Female', 'White', 'sweet', true)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 3, 'Male', 'Brown', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Grinch', 22, 'Male', 'Gray', 'grumpy', true)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 3, 'Male', 'Brown', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Wendy', 8, 'Female', 'Gray', 'calm', true)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 3, 'Male', 'Brown', 'calm', true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES(NULL, 3, 'Male', 'Brown', 'calm', true)
